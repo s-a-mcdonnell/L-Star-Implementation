7f2fc58cd324c6bba4d49f1be52bf306c5fd9867
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-06-27 15:58:38 -0400

    Add skeleton outline to methods

--- learner.py
+++ learner.py
@@ -1,2 +1,22 @@
-# Initialize T and ^M
-# Go through the loop
+from teacher import Teacher
+
+class Learner:
+
+    # Constructor
+    def __init__(self):
+        print("learner created")
+
+        # Create teacher
+        self.my_teacher = Teacher()
+
+        # Initialize T and ^M
+        pass
+
+    def run(self):
+        print("running")
+
+        unsolved = True
+
+        # Go through the loop
+        while unsolved:
+            pass
--- teacher.py
+++ teacher.py
@@ -1,3 +1,15 @@
-# equivalency query
+class Teacher:
 
-# membership query
+    # Constructor
+    def __init__(self):
+        print("teacher created")
+        pass
+
+
+    # equivalency query
+    def equivalent(self, m_hat):
+        pass
+
+    # membership query
+    def member(self, s):
+        pass
