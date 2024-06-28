a889a0b68f0144d1e463a9f82471dc15187231f9
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-06-28 12:40:38 -0400

    Test creation and editing tree

--- practice_dfa.py
+++ practice_dfa.py
@@ -10,11 +10,11 @@ def compare_dfas(teacher, dfa_to_compare):
 
 ############################################################################
 
-from teacher import Teacher
-import random
+from learner import Learner
+from learner import Node
 
 # The teacher will use the provided alphabet
-# NOTE: The alphabet can only contain single-character characters
+'''# NOTE: The alphabet can only contain single-character characters
 alphabet = ['a', 'b', 'c', '1', '2', '3']
 
 my_teacher = Teacher(alphabet, num_nodes = 5, seed = 1821)
@@ -25,12 +25,6 @@ my_dfa.append([])
 my_dfa[0].append(1)
 for character in alphabet:
     my_dfa[0].append(0)
-
-'''compare_dfas(my_teacher, my_dfa)
-
-print("-----")
-
-# My other DFA always rejects'''
 
 my_dfa_2 = []
 my_dfa_2.append([])
@@ -44,8 +38,19 @@ print("-----")
 
 
 my_teacher_2 = Teacher(alphabet, num_nodes = 4, seed = 1821)
-compare_dfas(my_teacher_2, my_dfa)
+compare_dfas(my_teacher_2, my_dfa)'''
 
+alphabet = ['0', '1']
+
+my_learner = Learner(alphabet)
+
+my_learner.t.root = Node("", None)
+
+my_learner.t.root.left_child = Node("left", my_learner.t.root)
+
+my_learner.t.root.right_child = Node("right", my_learner.t.root)
+
+my_learner.t.print_tree()
 
 
 
