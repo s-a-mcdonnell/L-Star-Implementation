2c77740e2aa0da396f06a9e8fd0c4bcf71323f9c
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-07-01 12:38:13 -0400

    Debugging comments

--- learner.py
+++ learner.py
@@ -151,13 +151,18 @@ class Learner:
             # sift gamma[i] in T
             access_string = self.sift(strng)
 
-            '''# check if the returned access string accepts or rejects in M and M_hat
+            # check if the returned access string accepts or rejects in M and M_hat
             # repeat loop until gamma[i] gives you differing results in M and M_hat (s[i] does not equal s_hat[i])
-            if self.my_teacher.member(access_string) != self.my_teacher.member(access_string, self.m_hat):
+            '''if self.my_teacher.member(access_string) != self.my_teacher.member(access_string, self.m_hat):
+                print("breaking loop")
                 break'''
             
             # Repeat loop until sifting and running the truncated string through M_hat lead to distinct states (different access strings/row indices in M_hat)
             if self.access_string_reference[access_string] != self.m_hat.index(Teacher.final_state(strng, self.m_hat, self.alphabet)):
+                print("Access string: " + access_string)
+                print("Access string reference #: " + str(self.access_string_reference[access_string]))
+                print("Final state: " + str(Teacher.final_state(strng, self.m_hat, self.alphabet)))
+                print("M_hat index: " + str(self.m_hat.index(Teacher.final_state(strng, self.m_hat, self.alphabet))))
                 print("breaking loop")
                 break
 
