941294ac8252bd2c775f14d7d9327f67b61a6eb9
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-07-01 14:15:08 -0400

    Create testing loop

--- practice_dfa.py
+++ practice_dfa.py
@@ -7,14 +7,18 @@ dfa_for_testing = [[0, 1, 4], [1, 3, 2], [1, 0, 1], [0, 2, 1], [0, 2, 0]]
 # Initialize learner
 my_learner = Learner(alphabet, dfa_for_testing)
 
-print("---")
+for i in range(2):
 
-my_learner.t.print_tree()
+    print("---")
 
-print("---")
+    my_learner.t.print_tree()
 
-m_hat = my_learner.construct_hypothesis()
+    print("---")
 
-print("---")
+    my_learner.m_hat = my_learner.construct_hypothesis()
 
-print(m_hat)
+    print(my_learner.m_hat)
+
+    print("---")
+
+    my_learner.update_tree(my_learner.my_teacher.equivalent(my_learner.m_hat))
