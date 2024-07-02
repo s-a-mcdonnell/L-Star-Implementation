6373d875578e01eab274e0db4d195b95280e1e04
s-a-mcdonnell <150218525+s-a-mcdonnell@users.noreply.github.com>
2024-07-02 12:26:06 -0400

    Still getting errors on larger/more complex DFAs

--- l-star-tester.py
+++ l-star-tester.py
@@ -5,7 +5,7 @@ alphabet = ['a', 'b']
 dfa_for_testing = [[0, 1, 4], [1, 3, 2], [1, 0, 1], [0, 2, 1], [0, 2, 0]]
 
 # Create learner
-my_learner = Learner(alphabet, dfa_for_testing)
+my_learner = Learner(alphabet)
 
 # Let learner run
 my_learner.lstar_algorithm()
